d5840cbb2cdf47e6e9ca9d89294c5f62dba78e44
Beanheo21 <benjaminjheo@gmail.com>
2026-06-19 20:35:34 -0400

    fix url

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ async function searchPlanet() {
   apodEl.className = "apod-section hidden";
 
   try {
-    const url = `https://corsproxy.io/?url=https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+pl_name,pl_rade,pl_masse,pl_eqt,pl_orbper,st_dist,disc_year,pl_bmasse+from+ps+where+default_flag=1+and+pl_name+like+'${encodeURIComponent(name).replace(/%20/g, '+')}
-'&format=json`;
+    const query = `select+pl_name,pl_rade,pl_masse,pl_eqt,pl_orbper,st_dist,disc_year+from+ps+where+default_flag=1+and+pl_name+like+'${name.replace(/ /g, '+')}'`;
+const url = `https://corsproxy.io/?url=${encodeURIComponent('https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=' + query + '&format=json')}`;
     const res = await fetch(url);
     const data = await res.json();
 
